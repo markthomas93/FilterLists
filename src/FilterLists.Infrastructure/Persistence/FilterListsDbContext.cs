8af799c7843d1e76bf8150ce8b3cf673214ef500
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-05-24 08:48:34 -0500

    convert DbSet to nullable reference types

--- src/FilterLists.Infrastructure/Persistence/FilterListsDbContext.cs
+++ src/FilterLists.Infrastructure/Persistence/FilterListsDbContext.cs
@@ -9,20 +9,20 @@ namespace FilterLists.Infrastructure.Persistence
         {
         }
 
-        public DbSet<Dependent> Dependents { get; set; }
-        public DbSet<FilterList> FilterLists { get; set; }
-        public DbSet<FilterListLanguage> FilterListLanguages { get; set; }
-        public DbSet<FilterListMaintainer> FilterListMaintainers { get; set; }
-        public DbSet<FilterListTag> FilterListTags { get; set; }
-        public DbSet<Fork> Forks { get; set; }
-        public DbSet<Language> Languages { get; set; }
-        public DbSet<License> Licenses { get; set; }
-        public DbSet<Maintainer> Maintainers { get; set; }
-        public DbSet<Merge> Merges { get; set; }
-        public DbSet<Software> Software { get; set; }
-        public DbSet<SoftwareSyntax> SoftwareSyntaxes { get; set; }
-        public DbSet<Syntax> Syntaxes { get; set; }
-        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Dependent> Dependents { get; set; } = null!;
+        public DbSet<FilterList> FilterLists { get; set; } = null!;
+        public DbSet<FilterListLanguage> FilterListLanguages { get; set; } = null!;
+        public DbSet<FilterListMaintainer> FilterListMaintainers { get; set; } = null!;
+        public DbSet<FilterListTag> FilterListTags { get; set; } = null!;
+        public DbSet<Fork> Forks { get; set; } = null!;
+        public DbSet<Language> Languages { get; set; } = null!;
+        public DbSet<License> Licenses { get; set; } = null!;
+        public DbSet<Maintainer> Maintainers { get; set; } = null!;
+        public DbSet<Merge> Merges { get; set; } = null!;
+        public DbSet<Software> Software { get; set; } = null!;
+        public DbSet<SoftwareSyntax> SoftwareSyntaxes { get; set; } = null!;
+        public DbSet<Syntax> Syntaxes { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

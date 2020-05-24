260518480441c86c4df72231b6a6cd027ccfe099
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-05-24 15:14:39 -0500

    fix some GraphQL DI wiring

--- src/FilterLists.Api/Startup.cs
+++ src/FilterLists.Api/Startup.cs
@@ -1,10 +1,12 @@
 using FilterLists.Api.GraphQL;
 using FilterLists.Application;
 using FilterLists.Infrastructure;
+using GraphQL;
 using GraphQL.Server;
 using GraphQL.Server.Ui.Playground;
 using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,13 +25,19 @@ namespace FilterLists.Api
         {
             services.AddInfrastructure(Configuration);
             services.AddApplication();
-            services.AddSingleton<ISchema, FilterListSchema>();
+            services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService!));
+            services.AddScoped<ISchema, FilterListSchema>();
             services.AddGraphQL().AddGraphTypes(ServiceLifetime.Scoped);
+            services.Configure<KestrelServerOptions>(o =>
+            {
+                //TODO: limitation of GraphQL using Newtonsoft.JSON
+                o.AllowSynchronousIO = true;
+            });
         }
 
         public static void Configure(IApplicationBuilder app)
         {
-            app.UseGraphQL<FilterListSchema>();
+            app.UseGraphQL<ISchema>();
             app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
         }
     }

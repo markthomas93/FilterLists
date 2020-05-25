030be21d4dd753ebdd39186e96c988061e4951db
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-05-25 07:35:35 -0500

    WIP add ODATA and API Versioning

--- svr/src/FilterLists.Api/Startup.cs
+++ svr/src/FilterLists.Api/Startup.cs
@@ -1,10 +1,13 @@
 using FilterLists.Application;
 using FilterLists.Infrastructure;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
 
 namespace FilterLists.Api
 {
@@ -24,7 +27,10 @@ namespace FilterLists.Api
             services.AddControllers().SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.Configure<ForwardedHeadersOptions>(o =>
                 o.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto);
-            services.AddOpenApiDocument();
+            services.AddApiVersioning();
+            services.AddOData().EnableApiVersioning();
+            services.AddVersionedApiExplorer(o => o.GroupNameFormat = "'v'VVV");
+            services.AddOpenApiDocument(s => s.PostProcess = d => { d.Info.Title = "FilterLists API"; });
         }
 
         public static void Configure(IApplicationBuilder app)
@@ -34,9 +40,21 @@ namespace FilterLists.Api
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
             app.UseRouting();
-            app.UseEndpoints(e => { e.MapControllers(); });
+            app.UseEndpoints(e =>
+            {
+                e.MapControllers();
+                e.Select().Filter().OrderBy().Count().MaxTop(10);
+                e.MapODataRoute("odata", "odata", GetEdmModel());
+            });
             app.UseOpenApi();
             app.UseSwaggerUi3();
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var odataBuilder = new ODataConventionModelBuilder();
+            odataBuilder.EntitySet<WeatherForecast>("WeatherForecast");
+            return odataBuilder.GetEdmModel();
         }
     }
 }

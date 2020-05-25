2c7f7620bcd52d986d3ea73d0c4e0c3ae7cb944b
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-05-25 16:03:36 -0500

    use IConfigureOptions for Swagger config

---
+++ svr/src/FilterLists.Api/Swagger/ConfigureSwaggerGenOptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace FilterLists.Api.Swagger
+{
+    public class ConfigureSwaggerGenOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        public void Configure(SwaggerGenOptions options)
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "FilterLists API",
+                Description =
+                    "The independent, comprehensive directory of filter and host lists for advertisements, trackers, malware, and annoyances.",
+                Version = "v1",
+                Contact = new OpenApiContact
+                {
+                    Name = "FilterLists",
+                    Url = new Uri("https://github.com/collinbarrett/FilterLists")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = $"Released under MIT License. ©{DateTime.Now.Year} Collin M. Barrett",
+                    Url = new Uri("https://github.com/collinbarrett/FilterLists/blob/master/LICENSE")
+                }
+            });
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlPath);
+        }
+    }
+}
--- svr/src/FilterLists.Api/Swagger/ServiceRegistration.cs
+++ svr/src/FilterLists.Api/Swagger/ServiceRegistration.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace FilterLists.Api.Swagger
 {
@@ -12,29 +11,8 @@ namespace FilterLists.Api.Swagger
     {
         public static void AddSwaggerGenCustom(this IServiceCollection services)
         {
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "FilterLists API",
-                    Description =
-                        "The independent, comprehensive directory of filter and host lists for advertisements, trackers, malware, and annoyances.",
-                    Version = "v1",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "FilterLists",
-                        Url = new Uri("https://github.com/collinbarrett/FilterLists")
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = $"Released under MIT License. ©{DateTime.Now.Year} Collin M. Barrett",
-                        Url = new Uri("https://github.com/collinbarrett/FilterLists/blob/master/LICENSE")
-                    }
-                });
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGenOptions>();
+            services.AddSwaggerGen();
         }
 
         public static void UseSwaggerCustom(this IApplicationBuilder app)

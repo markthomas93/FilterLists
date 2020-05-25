c7f3673b8d3f647860198c255a4b952566d687c0
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-05-25 14:24:49 -0500

    rm logger from demo controller

--- svr/src/FilterLists.Api/V1/WeatherForecastController.cs
+++ svr/src/FilterLists.Api/V1/WeatherForecastController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace FilterLists.Api.V1
 {
@@ -15,13 +14,6 @@ namespace FilterLists.Api.V1
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet]
         [EnableQuery]
